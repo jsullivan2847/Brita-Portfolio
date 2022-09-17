66a999f911485cee78c22acf14c19baa538fea32
James Sullivan <jsullivan2847@gmail.com>
2022-09-16 21:43:11 -0400

    home page background

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { Route } from 'react-router-dom';
 import Home from './pages/Home/home';
+import Corsets from './pages/Corsets/corsets';
 
 function App() {
   return (
     <div className="App">
       <Route exact path='/'>
         <Home/>
+      </Route>
+      <Route path='/Corsets'>
+        <Corsets/>
       </Route>
     </div>
   );
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
+import './home.css'
 
 export default function Home() {
   return (
     <div>
         <Header text="Brita Van Tol"/>
+        {/* <img className='egg' src="images/Egg.jpg" alt="" /> */}
     </div>
   )
 }
