2309c241e357798bc9e6630127f5d58de2b002e3
James Sullivan <jsullivan2847@gmail.com>
2022-09-20 12:09:46 -0400

    Added Professional Pictures

--- src/pages/Professional/professional.jsx
+++ src/pages/Professional/professional.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import Content from '../../components/Content/Content'
 import Hamburger from '../../components/Hamburger/Hamburger'
 import Header from '../../components/Header/Header'
+import Picture from '../../components/Picture/Picture'
 
 export default function Professional() {
     const textColor = "#FFFFFF"
@@ -10,6 +12,21 @@ export default function Professional() {
     <div className='page'>
       <Hamburger color={color}/>
       <Header image={image}color={color} textColor={textColor} text="Projects"/>
+      <Content>
+        <Picture url="/images/Professional/BlackLeather.png"/>
+        <Picture url="/images/Professional/BlueLace.png"/>
+        <Picture url="/images/Professional/FloralDress.png"/>
+        <Picture url="/images/Professional/FloralDress2.png"/>
+        <Picture url="/images/Professional/FloralDress3.png"/>
+        <Picture url="/images/Professional/RedLeather.png"/>
+        <Picture url="/images/Professional/BlackLeather2.png"/>
+        <Picture url="/images/Professional/Stiches.png"/>
+        <Picture url="/images/Professional/Stiches2.png"/>
+        <Picture url="/images/Professional/Strings.png"/>
+        <Picture url="/images/Professional/Strings2.png"/>
+        <Picture url="/images/Professional/WhiteDress.png"/>
+        <Picture url="/images/Professional/WhiteLace.png"/>
+      </Content>
     </div>
   )
 }
